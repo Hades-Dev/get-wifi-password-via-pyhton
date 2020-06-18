226280052f86ae59006b0cc7e1578699f9e0e13f
Mr.Hades <36410284+Hades-Dev@users.noreply.github.com>
2020-06-18 17:26:46 +0300

    Add files via upload

---
+++ get_wifi_passwords.py
@@ -0,0 +1,39 @@
+import subprocess
+import os
+data=subprocess.check_output(['netsh','wlan','show','profile']).decode('cp1252').split('\n')
+profiles=[i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+
+
+os.system('cls')
+path_file=os.path.dirname(os.path.abspath(__file__))
+
+header='''
+ Wifi Name                  wifi password      pass Length     
+--------------------------------------------------------------------\n'''
+f=open(path_file+"\saved_wifi_pass.txt","a")
+f.write(header)
+
+for i in profiles:
+    results=subprocess.check_output(['netsh','wlan','show','profile',i,
+                                     'key=clear']).decode('cp1252').split('\n')
+    results=[b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+
+    try:
+        psw_len = len(results[0])
+        if len(results[0])>0:
+            #print("{:25}  {:22}{}".format(i, results[0],psw_len))
+            get_data="{:25}  {:22}{}".format(i, results[0],psw_len)
+            #f = open("saved_wifi_pass.txt", "a")
+            f.write(get_data+"\n")
+    except IndexError:
+        continue
+
+print('''
+------------------------------------------------------------
+           File Has Been Saved Successfully !
+
+           This Script Was Made By FSOCIETY  | Version 1.5
+------------------------------------------------------------''')
+
+f.write("\n__________________________(Powered By FSOCIETY)_____________________\n\n\n")
+f.close()
